646e29be8f08733a2f21d12c9d660b1e4469e83c
Richa <richa008@gmail.com>
2020-04-12 18:08:01 -0700

    Adding Dice app

---
+++ Dicee/Dicee-iOS13/ViewController.swift
@@ -0,0 +1,33 @@
+//
+//  ViewController.swift
+//  Dicee-iOS13
+//
+//  Created by Angela Yu on 11/06/2019.
+//  Copyright © 2019 London App Brewery. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var diceImageView1: UIImageView!
+    @IBOutlet weak var diceImageView2: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+    @IBAction func rollButtonPressed(_ sender: Any) {
+        
+        let diceArray = [ #imageLiteral(resourceName: "DiceOne"), #imageLiteral(resourceName: "DiceTwo"), #imageLiteral(resourceName: "DiceThree"), #imageLiteral(resourceName: "DiceFour"), #imageLiteral(resourceName: "DiceFive"), #imageLiteral(resourceName: "DiceSix") ];
+        
+        let random = Int.random(in: 0...5);
+        
+        // Two ways of doing random
+        diceImageView1.image = diceArray.randomElement();
+        diceImageView2.image = diceArray[random];
+    }
+}
+
