3d8ae86b22b583024fac310bf8e80212ea7bf9c5
Richa <richa008@gmail.com>
2020-06-16 17:04:22 -0700

    Hotdog or not app

---
+++ HotdogOrNot/HotdogOrNot/ViewController.swift
@@ -0,0 +1,77 @@
+//
+//  ViewController.swift
+//  HotdogOrNot
+//
+//  Created by Richa Deshmukh on 5/29/20.
+//  Copyright © 2020 Richa Deshmukh. All rights reserved.
+//
+
+import UIKit
+import CoreML
+import Vision
+
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+
+    @IBOutlet weak var imageView: UIImageView!
+    
+    let imagePicker = UIImagePickerController()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        imagePicker.delegate = self
+        imagePicker.sourceType = .photoLibrary // .camera for camera
+        imagePicker.allowsEditing = false
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        if let userImagePicked = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
+            imageView.image = userImagePicked
+            
+            guard let ciimage = CIImage(image: userImagePicked) else {
+                fatalError("Could not convert UIImage to CIImage")
+            }
+            
+            detectImage(image: ciimage)
+        }
+        
+        imagePicker.dismiss(animated: true, completion: nil)
+    }
+    
+    func detectImage(image: CIImage) {
+        guard let model = try? VNCoreMLModel(for: Inceptionv3().model) else {
+            fatalError("Error loading model")
+        }
+        
+        let request = VNCoreMLRequest(model: model) { (request, error) in
+            guard let results = request.results as? [VNClassificationObservation] else {
+                fatalError("Model failed to process image")
+            }
+            
+            if let firstResult = results.first {
+                if firstResult.identifier.contains("hotdog") {
+                    self.navigationItem.title = "Hotdog!"
+                } else {
+                    self.navigationItem.title = "Not Hotdog!"
+                }
+                
+            }
+        }
+        
+        let handler = VNImageRequestHandler(ciImage: image)
+        
+        do {
+            try handler.perform([request])
+        } catch {
+            print(error)
+        }
+    }
+
+    @IBAction func cameraButtonPressed(_ sender: UIBarButtonItem) {
+        
+        present(imagePicker, animated: true, completion: nil)
+        
+    }
+    
+}
+
